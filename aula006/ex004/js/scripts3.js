fa162bac1460ae11ca4ced1deef8e059a183f884
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-04-08 19:19:44 -0300

    adicionando aula 006 de JS curso em video, falta adicionar o resto da aula 006

--- aula006/ex004/js/scripts3.js
+++ aula006/ex004/js/scripts3.js
@@ -1,3 +1,5 @@
 var nome = window.prompt('qual é seu nome?');
 document.write(`Olá, <strong>${nome}<strong>! seu nome tem ${nome.length} letras</br>`);
 document.write(`seu nome em maisculas é ${nome.toUpperCase()}`);
+
+// falta adicionar o resto da aula 006 no ex04
