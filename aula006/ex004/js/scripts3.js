4237c33600b0b1ce494d0a11b80bd3a4dbc4599b
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-06-11 17:53:53 -0300

    adicionando algumas coisinhas na aula 004 em js

--- aula006/ex004/js/scripts3.js
+++ aula006/ex004/js/scripts3.js
@@ -6,4 +6,7 @@ document.write(`seu nome em maisculas é ${nome.toUpperCase()}</br>`);
 
 document.write(`Seu nome em minusculas é ${nome.toLowerCase()}`);
 
+// n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) - Formata o número n1 (variavel) como moeda brasileira (R$), seguindo o padrão do português do Brasil, 
+// mas pode mudar conforme o muda o currency ali (moeda) por ex: USD, EUR //
+
 // teste... testando
